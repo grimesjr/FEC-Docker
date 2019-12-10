c92dd0a1309ed29ed68e4d06e5d307e235599727
Jimmy Lin <weevil31@hotmail.com>
2019-12-10 09:34:07 -0800

    updated port number

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const db = require('../database/model.js');
 const app = express();
-const port = 3000;
+const port = 3003;
 
 const bodyParser = require('body-parser');
 app.use(bodyParser.json());
